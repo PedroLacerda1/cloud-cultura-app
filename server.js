5650ddbc2a9a5b2470209d24433b1ceb4840acbc
PedroLacerda1 <pedrohenrilacerda@gmail.com>
2026-03-31 13:28:30 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Aplicação rodando na nuvem com CI/CD 🚀');
+});
+
+app.get('/status', (req, res) => {
+  res.json({
+    status: "OK",
+    mensagem: "Pipeline funcionando com sucesso"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
