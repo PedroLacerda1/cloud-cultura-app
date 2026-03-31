b585aea431e05909448689bcaaca526a408549bd
PedroLacerda1 <pedrohenrilacerda@gmail.com>
2026-03-31 18:06:07 -0300

    alterações

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Aplicação rodando na nuvem com CI/CD 🚀');
+  res.send('Aplicação rodando na nuvem com CI/CD 🚀😊');
 });
 
 app.get('/status', (req, res) => {
